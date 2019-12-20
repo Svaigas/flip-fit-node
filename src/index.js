4bd3f5a01903f8b3ada2e17003f0efc57b7e05bf
Svaigas <adamiecmateusz@wp.pl>
2019-12-20 22:38:16 +0100

    added basic mongo connection [FFN]

--- src/index.js
+++ src/index.js
@@ -5,8 +5,17 @@ import {} from 'dotenv/config'
 const app = express()
 const PORT = process.env.PORT || 3000
 
+import { MongoMemoryServer } from 'mongodb-memory-server'
+export const mongoServer = new MongoMemoryServer()
+import { createSchemas } from './db/index'
+// import mongoose from 'mongoose'
+// export const conn = mongoose.createConnection()
+
 app.use('/v1', routesV1)
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is listening on port: ${PORT}`)
+  const uri = await mongoServer.getConnectionString()
+  await createSchemas(uri)
+  console.log(`MongoDB server is runing at uri: ${uri}`)
 })
--- src/routes/api/v1/generic.js
+++ src/routes/api/v1/generic.js
@@ -1,8 +1,14 @@
+import { mongoServer } from '../../../index'
+// import mongoose from 'mongoose'
+
 /**
  * Add two numbers.
  * @param {number} req The API request
  * @param {number} res The API response
  */
-export function findById(req, res) {
-  res.send('carts')
+export async function findById(req, res) {
+  const { params: { id, type } } = req
+  console.log(mongoServer.getInstanceInfo())
+
+  res.send(`id ${id} type ${type}`)
 }
